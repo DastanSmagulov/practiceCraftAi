92fc59982609cf172df60bdb4d482f480242790c
DastanSmagulov <smagulovdastan07@gmail.com>
2024-07-13 16:51:00 +0500

    fix: fix deployment bugs

--- src/app/createProject/page.tsx
+++ src/app/createProject/page.tsx
@@ -24,11 +24,11 @@ const CreateProject = () => {
     }
   }, []);
 
-  const handleChange = (e) => {
+  const handleChange = (e: any) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
     try {
       if (!user) {
